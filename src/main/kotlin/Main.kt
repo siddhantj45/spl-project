71fe47189b5dc31232347b298e0eb0a8853c4d31
Siddhant Jaiswal <siddhant45j@gmail.com>
2023-11-09 14:25:30 -0500

    Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,34 @@
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
+import org.openqa.selenium.By
+import java.time.Duration
+
+class QuoraScraper {
+
+    fun createDriver(): WebDriver {
+        println("Enter a Quora Username : ")
+        val username = readLine()
+        val driver = ChromeDriver()
+        driver.manage().window().maximize()
+        driver.get("https://www.quora.com/profile/" + username + "/answers")
+        val amtsecs = Duration.ofSeconds(10)
+        WebDriverWait(driver, amtsecs).until(
+            ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@class=\"q-box qu-borderBottom\"]"))
+        )
+        return driver
+    }
+
+
+}
+
+
+fun main(args: Array<String>) {
+
+    println("Hello World!")
+    val scraper = QuoraScraper()
+    val driver = scraper.createDriver()
+
+
+}
